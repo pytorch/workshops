522a77b9327427330c2589ae839f665dc7fa8967
Brad Heintz <bradheintz@fb.com>
2020-07-07 15:19:26 -0700

    pt mobile hackathon walkthru projects

---
+++ PTMobileWalkthruIOS/PTMobileDemo/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  PTMobileDemo
+//
+//  Created by Brad Heintz on 6/23/20.
+//  Copyright © 2020 Brad Heintz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    private lazy var module: TorchModule = {
+        if let filePath = Bundle.main.path(forResource: "model", ofType: "pt"),
+            let module = TorchModule(fileAtPath: filePath) {
+            return module
+        } else {
+            fatalError("Can't find the model file!")
+        }
+    }()
+    
+    
+    private lazy var labels: [String] = {
+        if let filePath = Bundle.main.path(forResource: "words", ofType: "txt"),
+            let labels = try? String(contentsOfFile: filePath) {
+            return labels.components(separatedBy: .newlines)
+        } else {
+            fatalError("Can't find the text file!")
+        }
+    }()
+
+    @IBAction func doInference(_ sender: Any) {
+        let image = UIImage(named: "kitten.jpg")!
+        imageView.image = image
+        let resizedImage = image.resized(to: CGSize(width: 224, height: 224))
+
+        guard var pixelBuffer = resizedImage.normalized() else {
+            return
+        }
+        guard let outputs = module.predict(image: UnsafeMutableRawPointer(&pixelBuffer)) else {
+            return
+        }
+        let zippedResults = zip(labels.indices, outputs)
+        let sortedResults = zippedResults.sorted { $0.1.floatValue > $1.1.floatValue }.prefix(3)
+        var text = ""
+        for result in sortedResults {
+            text += "\u{2022} \(labels[result.0]) \n\n"
+        }
+        textView.text = text
+    }
+}
+
